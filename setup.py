779d75c855fab444252ed4bbd03846427be24322
Jarred Barber <jpb5082@gmail.com>
2017-09-07 15:39:13 -0400

    Skeleton package

---
+++ setup.py
@@ -0,0 +1,11 @@
+from setuptools import setup
+
+setup(name='BetaSNES',
+      version='0.1',
+      description='Beta-distribution variant of Natural Evolution Strategies',
+      url='http://github.com/jarredbarber/BetaSNES',
+      author='Jarred Barber',
+      author_email='jpb5082@gmail.com',
+      license='MIT',
+      packages=['nes_optim'],
+      zip_safe=False)
